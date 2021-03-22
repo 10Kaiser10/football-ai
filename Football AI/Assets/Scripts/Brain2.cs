430bbfb5bf68dd7c0af7ed0ee4e7308b1e5997f8
10Kaiser10 <57315492+10Kaiser10@users.noreply.github.com>
2021-03-22 19:30:15 +0530

    Implemented Neural network and Genetic Algorithm

---
+++ Football AI/Assets/Scripts/Brain2.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain2 : MonoBehaviour
+{
+    private float timer;
+    private float fulltimer;
+    [System.NonSerialized]
+    public float simTime;
+
+    //for score calc
+    private float score;
+    public bool reachedTarget;
+    private float minimumDist;
+
+    //target
+    [System.NonSerialized]
+    public Vector3 target;
+    [System.NonSerialized]
+    public Vector3 targetVel;
+
+    //body
+    public GameObject body;
+    private Transform bodyTrans;
+    private Rigidbody bodyRb;
+    private CapsuleCollider bodyColl;
+    public float MaxForce = 10;
+
+    //neural network
+    [System.NonSerialized]
+    public int inputNodes;
+    [System.NonSerialized]
+    public int outputNodes;
+    [System.NonSerialized]
+    public int[] hiddenLayersNodes;
+    [System.NonSerialized]
+    public int[] LayersNodes;
+    [System.NonSerialized]
+    public float[][,] weightsNBiases;
+
+    void Start()
+    {
+        bodyTrans = body.transform;
+        bodyRb = body.GetComponent<Rigidbody>();
+        bodyColl = body.GetComponent<CapsuleCollider>();
+        timer = 0;
+        fulltimer = 0;
+        reachedTarget = false;
+        minimumDist = 1000;
+    }
+
+    float putInBetween(float l, float h, float n)
+    {
+        float diff = h - l;
+        return l + diff * n;
+    }
+
+    float leakyRelu(float input)
+    {
+        if (input > 0)
+        {
+            return input;
+        }
+        else
+        {
+            return 0.01f * input;
+        }
+    }
+
+    float[] leakyRelu(float[] input)
+    {
+        float[] output = new float[input.Length + 1];
+        output[0] = 1;
+
+        for (int i = 1; i <= input.Length; i++)
+        {
+            output[i] = leakyRelu(input[i - 1]);
+        }
+
+        return output;
+    }
+
+    float[] matrixMult(float[,] matrix, float[] vector)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        float[] output = new float[rows];
+
+        for (int i = 0; i < rows; i++)
+        {
+            output[i] = 0;
+            for (int j = 0; j < cols; j++)
+            {
+                output[i] += matrix[i, j] * vector[j];
+            }
+        }
+
+        return output;
+    }
+
+    float logisticAct(float input)
+    {
+        return 1 / (1 + Mathf.Exp(-input));
+    }
+
+    float[] logisticAct(float[] input)
+    {
+        for (int i = 0; i < input.Length; i++)
+        {
+            input[i] = logisticAct(input[i]);
+        }
+        return input;
+    }
+
+    float[] NeuralNetwork(float[] input)
+    {
+        float[] currentVector;
+        currentVector = new float[input.Length + 1];
+        currentVector[0] = 1;
+
+        for (int i = 1; i <= input.Length; i++)
+        {
+            currentVector[i] = input[i - 1];
+        }
+
+        for (int i = 0; i < hiddenLayersNodes.Length; i++)
+        {
+            currentVector = matrixMult(weightsNBiases[i], currentVector);
+            currentVector = leakyRelu(currentVector);
+        }
+
+        return logisticAct(matrixMult(weightsNBiases[hiddenLayersNodes.Length], currentVector));
+    }
+
+    void move()
+    {
+        float[] inputVec = new float[inputNodes];
+
+        inputVec[0] = target.x;
+        inputVec[1] = target.z;
+        inputVec[2] = targetVel.x;
+        inputVec[3] = targetVel.z;
+
+        inputVec[4] = bodyTrans.localPosition.x;
+        inputVec[5] = bodyTrans.localPosition.z;
+        inputVec[6] = bodyRb.velocity.x;
+        inputVec[7] = bodyRb.velocity.z;
+
+        float[] outputArr = NeuralNetwork(inputVec);
+
+        Vector3 force = new Vector3(putInBetween(-MaxForce, MaxForce, outputArr[0]), 0, putInBetween(-MaxForce, MaxForce, outputArr[1]));
+
+        bodyRb.AddRelativeForce(force);
+    }
+
+    void calcScore()
+    {
+        if (reachedTarget == false && bodyColl.bounds.Contains(target) == true)
+        {
+            float timeScore = simTime - fulltimer;
+            float velocityAlignScore = Vector3.Dot(targetVel, bodyRb.velocity) / (Vector3.Magnitude(targetVel) * Vector3.Magnitude(bodyRb.velocity));
+            float dirAlignScore = Vector3.Dot(targetVel, target - bodyTrans.position) / (Vector3.Magnitude(targetVel) * Vector3.Magnitude(target - bodyTrans.position));
+            float velocityMagScore = 1 / (1 + Mathf.Pow((Vector3.Magnitude(targetVel) - Vector3.Magnitude(bodyRb.velocity)), 2));
+
+            reachedTarget = true;
+            score = 2 + timeScore + velocityAlignScore + dirAlignScore + velocityMagScore;
+        }
+        else if(reachedTarget == false)
+        {
+            float thisDist = Vector3.Distance(target, bodyTrans.position);
+            minimumDist = Mathf.Min(thisDist, minimumDist);
+        }
+    }
+
+    public float getScore()
+    {
+        if(reachedTarget == true)
+        {
+            return score;
+        }
+        else
+        {
+            float distanceScore = - minimumDist;
+            float speedScore = 15 / (1 + Mathf.Pow((Vector3.Magnitude(targetVel) - Vector3.Magnitude(bodyRb.velocity)), 2));
+            return -15 + distanceScore + speedScore;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        fulltimer += Time.deltaTime;
+        timer += Time.deltaTime;
+        if (timer > 0.02)
+        {
+            move();
+            calcScore();
+            timer = 0;
+        }
+    }
+}
---
+++ Football AI/Assets/Scripts/Evolver2.cs
@@ -0,0 +1,264 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Evolver2 : MonoBehaviour
+{
+    //timer
+    private float timer;
+    public float simDuration = 10;
+    public int generationsPerCycle = 8;
+    private int generationNum = 1;
+
+    //evolution
+    //public float groundTouchWieght = 1;
+    //public float distanceWieght = 1;
+    public float mutationNum = 1;
+
+    //population
+    public int populationSize = 16;
+    private GameObject[] population;
+    private float[] scores;
+
+    //spawning
+    public int spawnRows = 20;
+    public GameObject prefab;
+    public GameObject parent;
+    public float ydefault = 0.5f;
+    public float spawnRadius = 15;
+    public float targetSpeedRange = 10;
+
+    //neural network
+    private int inputNodes = 8;
+    private int outputNodes = 2;
+    private int[] hiddenLayersNodes = { };
+    private int[] LayersNodes = { 8, 2 };
+    private float[][][,] weightsNBiases;
+    private float[][][,] nextWeightsNBiases;
+    private Vector2 initiantionRange = new Vector2(-5f, 5f);
+
+    public void Begin()
+    {
+        scores = new float[populationSize];
+        weightsNBiases = new float[populationSize][][,];
+
+        for (int i = 0; i < populationSize; i++)
+        {
+            weightsNBiases[i] = new float[hiddenLayersNodes.Length + 1][,];
+
+            for (int l = 0; l <= hiddenLayersNodes.Length; l++)
+            {
+                int rows = LayersNodes[l + 1];
+                int cols = LayersNodes[l] + 1;
+                weightsNBiases[i][l] = new float[rows, cols];
+
+                for (int j = 0; j < rows; j++)
+                {
+                    for (int k = 0; k < cols; k++)
+                    {
+                        weightsNBiases[i][l][j, k] = Random.Range(initiantionRange.x, initiantionRange.y);
+                    }
+                }
+            }
+        }
+
+        population = new GameObject[populationSize];
+
+        for (int i = 0; i < populationSize; i++)
+        {
+            Vector3 spawnPos = new Vector3(10 * (i / spawnRows) + Random.Range(-spawnRadius, spawnRadius), 0, 10 * (i % spawnRows) + Random.Range(-spawnRadius, spawnRadius));
+            Vector3 spawnVel = new Vector3(Random.Range(-targetSpeedRange, targetSpeedRange), 0, Random.Range(-targetSpeedRange, targetSpeedRange));
+            population[i] = Instantiate(prefab, spawnPos, Quaternion.identity, parent.transform);
+            population[i].transform.GetChild(1).GetComponent<Rigidbody>().velocity = spawnVel;
+            Brain2 objBrain = population[i].transform.GetChild(0).GetComponent<Brain2>();
+            objBrain.inputNodes = inputNodes;
+            objBrain.outputNodes = outputNodes;
+            objBrain.hiddenLayersNodes = hiddenLayersNodes;
+            objBrain.LayersNodes = LayersNodes;
+            objBrain.weightsNBiases = weightsNBiases[i];
+            objBrain.target = new Vector3(10 * (i / spawnRows) + Random.Range(-spawnRadius, spawnRadius), 0.5f, 10 * (i % spawnRows) + Random.Range(-spawnRadius, spawnRadius));
+            objBrain.targetVel = new Vector3(Random.Range(-targetSpeedRange, targetSpeedRange), 0, Random.Range(-targetSpeedRange, targetSpeedRange));
+            objBrain.simTime = simDuration;
+
+            population[i].transform.GetChild(2).transform.position = objBrain.target;
+        }
+    }
+
+    private void Start()
+    {
+        timer = 0;
+        Begin();
+    }
+
+    private void calcScores()
+    {
+        for (int i = 0; i < populationSize; i++)
+        {
+            scores[i] = population[i].transform.GetChild(0).GetComponent<Brain2>().getScore();
+        }
+    }
+
+    private void selection()
+    {
+        int avgFit = 0, sqsum = 0, numCrossed = 0;
+        foreach (int item in scores) { avgFit += item; }
+        avgFit /= populationSize;
+        foreach (int item in scores) { sqsum += (item - avgFit)*(item - avgFit); }
+        sqsum /= populationSize;
+        foreach (GameObject item in population) { numCrossed += (item.transform.GetChild(0).GetComponent<Brain2>().reachedTarget) ? 1:0; }
+        Debug.Log(generationNum.ToString() + " : " + avgFit.ToString() + " : " + sqsum.ToString() + " : " + numCrossed.ToString());
+
+        System.Array.Sort(scores, weightsNBiases);
+
+        nextWeightsNBiases = new float[populationSize][][,];
+
+        for (int i = populationSize-1; i >= populationSize/2; i--)
+        {
+            //nextWeightsNBiases[i] = new float[hiddenLayersNodes.Length + 1][,];
+            nextWeightsNBiases[populationSize - i - 1] = new float[hiddenLayersNodes.Length + 1][,];
+
+            for (int l = 0; l <= hiddenLayersNodes.Length; l++)
+            {
+                int rows = LayersNodes[l + 1];
+                int cols = LayersNodes[l] + 1;
+                //nextWeightsNBiases[i][l] = new float[rows, cols];
+                nextWeightsNBiases[populationSize - i - 1][l] = new float[rows, cols];
+
+                for (int j = 0; j < rows; j++)
+                {
+                    for (int k = 0; k < cols; k++)
+                    {
+                        //nextWeightsNBiases[i][l][j, k] = weightsNBiases[i][l][j, k];
+                        nextWeightsNBiases[populationSize - i - 1][l][j, k] = weightsNBiases[i][l][j, k];
+                    }
+                }
+            }
+        }
+    }
+
+    private void crossover()
+    {
+        for (int i = populationSize/2; i < populationSize; i++)
+        {
+            nextWeightsNBiases[i] = new float[hiddenLayersNodes.Length + 1][,];
+            int par1 = Random.Range(0, populationSize/2 - 1), par2 = Random.Range(0, populationSize / 2 - 1);
+
+            for (int l = 0; l <= hiddenLayersNodes.Length; l++)
+            {
+                int rows = LayersNodes[l + 1];
+                int cols = LayersNodes[l] + 1;
+                nextWeightsNBiases[i][l] = new float[rows, cols];
+
+                for (int j = 0; j < rows; j++)
+                {
+                    for (int k = 0; k < cols; k++)
+                    {
+                        float coeff = Random.Range(-1f, 1f);
+                        nextWeightsNBiases[i][l][j, k] = (0.5f - coeff) * nextWeightsNBiases[par1][l][j, k] + (0.5f + coeff) * nextWeightsNBiases[par2][l][j, k];
+                    }
+                }
+            }
+        }
+    }
+
+    private void randomise()
+    {
+        for (int i = 1; i < populationSize; i++)
+        {
+            nextWeightsNBiases[i] = new float[hiddenLayersNodes.Length + 1][,];
+
+            for (int l = 0; l <= hiddenLayersNodes.Length; l++)
+            {
+                int rows = LayersNodes[l + 1];
+                int cols = LayersNodes[l] + 1;
+                nextWeightsNBiases[i][l] = new float[rows, cols];
+
+                for (int j = 0; j < rows; j++)
+                {
+                    for (int k = 0; k < cols; k++)
+                    {
+                        nextWeightsNBiases[i][l][j, k] = Random.Range(initiantionRange.x, initiantionRange.y);
+                    }
+                }
+            }
+        }
+    }
+
+    private void respawn()
+    {
+        foreach (GameObject obj in population)
+        {
+            Destroy(obj);
+        }
+
+        population = new GameObject[populationSize];
+
+        for (int i = 0; i < populationSize; i++)
+        {
+            Vector3 spawnPos = new Vector3(10 * (i / spawnRows) + Random.Range(-spawnRadius, spawnRadius), 0, 10 * (i % spawnRows) + Random.Range(-spawnRadius, spawnRadius));
+            Vector3 spawnVel = new Vector3(Random.Range(-targetSpeedRange, targetSpeedRange), 0, Random.Range(-targetSpeedRange, targetSpeedRange));
+            population[i] = Instantiate(prefab, spawnPos, Quaternion.identity, parent.transform);
+            population[i].transform.GetChild(1).GetComponent<Rigidbody>().velocity = spawnVel;
+            Brain2 objBrain = population[i].transform.GetChild(0).GetComponent<Brain2>();
+            objBrain.inputNodes = inputNodes;
+            objBrain.outputNodes = outputNodes;
+            objBrain.hiddenLayersNodes = hiddenLayersNodes;
+            objBrain.LayersNodes = LayersNodes;
+            objBrain.weightsNBiases = weightsNBiases[i];
+            objBrain.target = new Vector3(10 * (i / spawnRows) + Random.Range(-spawnRadius, spawnRadius), 0.5f, 10 * (i % spawnRows) + Random.Range(-spawnRadius, spawnRadius));
+            objBrain.targetVel = new Vector3(Random.Range(-targetSpeedRange, targetSpeedRange), 0, Random.Range(-targetSpeedRange, targetSpeedRange));
+            objBrain.simTime = simDuration;
+
+            population[i].transform.GetChild(2).transform.position = objBrain.target;
+        }
+    }
+
+    private void mutation()
+    {
+        float dimentions = 0;
+        for( int i=1; i<LayersNodes.Length; i++)
+        {
+            dimentions += (1 + LayersNodes[i - 1]) * LayersNodes[i];
+        }
+        float mutationProb = mutationNum / 100;
+
+        for (int i = 0; i < populationSize; i++)
+        {
+            for (int l = 0; l <= hiddenLayersNodes.Length; l++)
+            {
+                int rows = LayersNodes[l + 1];
+                int cols = LayersNodes[l] + 1;
+
+                for (int j = 0; j < rows; j++)
+                {
+                    for (int k = 0; k < cols; k++)
+                    {
+                        if(Random.Range(0f,1f) <= mutationProb)
+                        {
+                            nextWeightsNBiases[i][l][j, k] += Random.Range(-0.5f, 0.5f);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > simDuration)
+        {
+            timer = 0;
+            calcScores();
+            selection();
+            crossover();
+            mutation();
+
+            weightsNBiases = nextWeightsNBiases;
+
+            respawn();
+            generationNum++;
+        }
+    }
+}
